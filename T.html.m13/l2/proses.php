1d4b5fc601115ef8b4627fd3e4fe345d5c6da816
alpino-droid <alpino.droid@gmail.com>
2026-04-03 09:35:59 +0700

    tgs

--- T.html.m13/l2/proses.php
+++ T.html.m13/l2/proses.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_POST['login'])) {
+if (isset($_POST['input'])) {
     $user = $_POST['nama'];
     $pass = $_POST['password'];
-    if ($user == "smkn4malang" && $pass == "123") {
+    if ($user == "samkn4malang" && $pass == "123") {
         echo "<h2>Login Berhasil</h2>";
     } else {
         echo "<h2>Login gagal</h2>";
--- T.html.m13/l3/proses.php
+++ T.html.m13/l3/proses.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['input'])) {
+if (isset($_POST['inout'])) {
     $jurusan = $_POST['jurusan'];
     echo "Jurusan Anda Adalah
     <B><font color='red'>$jurusan</font></B>";
